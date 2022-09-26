369b3b1851d4fddd975b49f051d4fe5687979e82
MAXUT0CHKA <101568642+MAXUT0CHKA@users.noreply.github.com>
2022-09-26 03:08:49 +0300

    Add files via upload

---
+++ 3-10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void last_digits(int n, int a[])
+{
+    for (int i = 0; i < n; ++i)
+    {
+        a[i] = a[i] % 10;
+    }
+}
+
+int main()
+{
+    int size;
+    scanf("%i", &size);
+    int array[size];
+    for (int i = 0; i < size; ++i)
+    {
+        int n;
+        scanf("%i", &n);
+        array[i] = n;
+    }
+    last_digits(size, array);
+    for (int i = 0; i < size; ++i)
+        printf("%i ", array[i]);
+    printf("\n");
+}
